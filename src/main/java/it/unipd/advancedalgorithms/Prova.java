80a5bcdeeaf123b26d3557632394cd460dbe4cfa
maga97 <magarotto.francesco@gmail.com>
2020-04-09 20:24:43 +0200

    Aggiunta scrittura su CSV

--- src/main/java/it/unipd/advancedalgorithms/Prova.java
+++ src/main/java/it/unipd/advancedalgorithms/Prova.java
@@ -1,10 +1,18 @@
 package it.unipd.advancedalgorithms;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Stream;
+
+import com.opencsv.CSVWriter;
 
 import it.unipd.advancedalgorithms.graph.*;
 import it.unipd.advancedalgorithms.algorithms.*;
@@ -12,35 +20,44 @@ import it.unipd.advancedalgorithms.algorithms.*;
 public class Prova {
 
   public static void main(final String[] args) throws Exception {
-    HashMap<Integer, Long> kruskalU = new HashMap<>();
-    HashMap<Integer, Long> kruskal = new HashMap<>();
-    HashMap<Integer, Long> prim = new HashMap<>();
+    List<String[]> kruskalUnionFindTimes = new ArrayList<String[]>();
+    List<String[]> kruskalTimes = new ArrayList<>();
+    List<String[]> primTimes = new ArrayList<>();
     try (Stream<Path> paths = Files.walk(Paths.get("datasets"))) {
       paths.filter(Files::isRegularFile).forEach(file -> {
         Graph g = GraphReader.getGraph("datasets/" + file.getFileName().toString());
-        int numberEdges = g.getEdges().size();
-        long primStartTime = System.nanoTime();
+        Integer numberEdges = g.getEdges().size();
+        Long primStartTime = System.nanoTime();
         Prim.solve(g, 1);
-        long primEndtTime = primStartTime - System.nanoTime();
-        prim.put(numberEdges, primEndtTime);
+        Long primEndtTime = primStartTime - System.nanoTime();
+        primTimes.add(new String[] {numberEdges.toString(), primEndtTime.toString()});
 
-        long kruskalStartTime = System.nanoTime();
+        Long kruskalStartTime = System.nanoTime();
         Kruskal.MST(g);
-        long kruskalEndtTime = kruskalStartTime - System.nanoTime();
-        kruskal.put(numberEdges, kruskalEndtTime);
+        Long kruskalEndtTime = kruskalStartTime - System.nanoTime();
+        kruskalTimes.add(new String[] {numberEdges.toString(), kruskalEndtTime.toString()});
 
         long kruskalUStartTime = System.nanoTime();
         KruskalUnionFind.KruskalMST(g);
         Long kruskalUEndtTime = kruskalUStartTime - System.nanoTime();
-        kruskalU.put(numberEdges, kruskalUEndtTime);
+        kruskalUnionFindTimes.add(new String[] {numberEdges.toString(), kruskalUEndtTime.toString()});
 
       });
     } catch (Exception e) {
     }
 
+    printFile("kruskal.csv", kruskalTimes);
+    printFile("prim.csv", primTimes);
+    printFile("unionkruskal.csv", kruskalUnionFindTimes);
   }
 
-  static void printEdge(Edge e) {
-    System.out.println(e.getStart() + " " + e.getEnd() + " " + e.getWeight());
+  private static void printFile(String filename, List<String[]> entries) throws IOException {
+    Files.deleteIfExists(Paths.get(filename));
+    try (FileOutputStream fos = new FileOutputStream(filename);
+                OutputStreamWriter osw = new OutputStreamWriter(fos, 
+                        StandardCharsets.UTF_8);
+                CSVWriter writer = new CSVWriter(osw, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END)) {
+            writer.writeAll(entries);
+        }
   }
 }
