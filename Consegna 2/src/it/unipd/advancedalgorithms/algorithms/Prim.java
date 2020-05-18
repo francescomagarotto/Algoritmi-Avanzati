a7f9d48d9f24b9fd3ca36e90daa94fd8645792a9
Enrico <enrico_97@hotmail.it>
2020-05-18 17:27:45 +0200

    copiate classi utili

---
+++ Consegna 2/src/it/unipd/advancedalgorithms/algorithms/Prim.java
@@ -0,0 +1,101 @@
+package it.unipd.advancedalgorithms.algorithms;
+
+import it.unipd.advancedalgorithms.graph.Edge;
+import it.unipd.advancedalgorithms.graph.Graph;
+import it.unipd.advancedalgorithms.graph.Heap;
+
+import java.util.*;
+
+public class Prim {
+    private static Map<Integer, Integer> key;
+
+    public static int solve(Graph G, int s) {
+        key = new HashMap<>();
+        Map<Integer, Integer> parent = new HashMap<>();
+        Set<Integer> Q = new HashSet<>(); // contains all the nodes not in the MST
+        Heap minHeap = new Heap(G.getnVertex(), new NodeComparator());
+
+        // initialize data structures
+        for (Integer id : G.getAdjacencyLists().keySet()) {
+            key.put(id, Integer.MAX_VALUE);
+            parent.put(id, -1);
+            Q.add(id);
+            minHeap.add(id);
+        }
+
+        key.replace(s, 0);
+
+        while (!Q.isEmpty()) {
+            Integer u = minHeap.poll(); // get the node with the smallest key
+            Q.remove(u);
+
+            for (Edge edge : G.getAdjacencyLists().get(u)) { // cycle all the edges adjacent to the node u
+                Integer v = edge.getEnd();
+                if (Q.contains(v) && edge.getWeight() < key.get(v)) {
+                    // if the new edge is lighter update parent and key for the node v
+                    parent.replace(v, u);
+                    key.replace(v, edge.getWeight());
+                    // update the heap
+                    minHeap.update(v);
+                }
+            }
+        }
+
+        int totalCost = 0;
+        for (Integer node : key.keySet()) {
+            if (node != s)
+                totalCost += key.get(node);
+        }
+
+
+        List<Edge> A = new LinkedList<>(); // minimum spanning tree
+        for (Integer node : key.keySet()) {
+            if (node != s)
+                A.add(new Edge(node, parent.get(node), key.get(node)));
+        }
+        //return A;
+
+        return totalCost;
+    }
+
+    // dumb implementation
+    public static List<Edge> dumbSolve(Graph G, int s) {
+        List<Edge> A = new ArrayList<>(); // List of edges that form the minimum spanning tree
+        Set<Integer> X = new HashSet<>(); // Set of nodes that have been visited
+        X.add(s);
+
+        Edge lightEdge;
+        do {
+            // reset current light edge
+            lightEdge = new Edge(-1, -1, Integer.MAX_VALUE);
+
+            // find light edge
+            for (Edge e : G.getEdges()) {
+                if (X.contains(e.getStart()) && !X.contains(e.getEnd())) {
+                    if (e.getWeight() < lightEdge.getWeight()) {
+                        lightEdge = e;
+                    }
+                }
+            }
+
+            if (lightEdge.getStart() != -1) {
+                // add light edge to A and update X
+                A.add(lightEdge);
+                X.add(lightEdge.getEnd());
+            }
+
+        } while (lightEdge.getStart() != -1); // there is no light edge
+
+        return A;
+    }
+
+    // Used in minHeap to sort the nodes by key
+    private static class NodeComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer n1, Integer n2) {
+            Integer key1 = key.get(n1);
+            Integer key2 = key.get(n2);
+            return key1.compareTo(key2);
+        }
+    }
+}
---
+++ Consegna 2/src/it/unipd/advancedalgorithms/graph/Graph.java
@@ -0,0 +1,60 @@
+package it.unipd.advancedalgorithms.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+    private List<Edge> edges;
+    private HashMap<Integer, LinkedList<Edge>> adjacencyLists;
+
+    public Graph(List<Edge> edgeList, HashMap<Integer, LinkedList<Edge>> adjList) {
+        edges = edgeList;
+        adjacencyLists = adjList;
+    }
+
+    public Graph() {
+        edges = new ArrayList<>();
+        adjacencyLists = new HashMap<>();
+    }
+
+    // add weighted edge from source to destination
+    public void addEdge(int src, int dest, int weight) {
+        if (adjacencyLists.containsKey(src))
+            adjacencyLists.get(src).add(new Edge(src, dest, weight));
+        else {
+            LinkedList<Edge> l = new LinkedList<>();
+            l.add(new Edge(src, dest, weight));
+            adjacencyLists.put(src, l);
+        }
+        if (adjacencyLists.containsKey(dest))
+            adjacencyLists.get(dest).add(new Edge(dest, src, weight));
+        else {
+            LinkedList<Edge> l = new LinkedList<>();
+            l.add(new Edge(dest, src, weight));
+            adjacencyLists.put(dest, l);
+        }
+    }
+
+    public Integer getnVertex() {
+        return adjacencyLists.keySet().size();
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public HashMap<Integer, LinkedList<Edge>> getAdjacencyLists() {
+        return adjacencyLists;
+    }
+
+    public void setAdjacencyLists(HashMap<Integer, LinkedList<Edge>> adjacenyLists) {
+        this.adjacencyLists = adjacenyLists;
+    }
+
+}
